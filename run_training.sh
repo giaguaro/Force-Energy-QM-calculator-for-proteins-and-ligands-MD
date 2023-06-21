d80da0b25b59a1f663f7d004bd27273267c78a59
Hazem Mslati <32345512+giaguaro@users.noreply.github.com>
2023-06-21 01:31:24 -0400

    Add files via upload

---
+++ run_training.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=train
+#SBATCH --mem=0
+
+source ~/.bashrc
+source activate nequip
+
+
+export LD_LIBRARY_PATH=/home/${USER}/.conda/envs/diffdock/lib/:$LD_LIBRARY_PATH
+
+export OPENBLAS_NUM_THREADS=2
+
+rm -r results && nequip-train ./spice_config.yaml
